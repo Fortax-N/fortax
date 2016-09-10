a65d71c4724f7befa179c68ce997835b1d9c559f
sthpravin <sthpravin@gmail.com>
2016-09-10 18:47:45 +0545

    Active admin installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   #route for homepage
   root 'static_pages#index'
   get '/online_estimate', to: 'online_estimate#index'
