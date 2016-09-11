746e339f2427d69c5cd9ec10fa5d945e0a807f74
sthpravin <sthpravin@gmail.com>
2016-09-11 14:06:23 +0545

    unnecessary comments in company.rb and active_admin.rb removed

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -1,18 +1,5 @@
 ActiveAdmin.register Company, as: "Company Information" do
 config.filters = false
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
 
  permit_params :about_us, :address_line_1, :address_line_2, :city, :state, :country, :phone_number, :email, :office_hours
 
