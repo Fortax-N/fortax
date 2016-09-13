8f9bd5b402f33aa1e2d6d2919485e09418f1bad1
sthpravin <sthpravin@gmail.com>
2016-09-14 02:18:40 +0545

    Company information displayed from company model

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -1,36 +1,22 @@
 ActiveAdmin.register Company, as: "Company Information" do
 config.filters = false
-
+actions :index, :show, :update, :edit
+ 
  permit_params :about_us, :address_line_1, :address_line_2, :city, :state, :country, :phone_number, :email, :office_hours
 
   index do
     selectable_column
     id_column
-	column :about_us
-	column :address_line_1
-	column :address_line_2
-	column :city
-	column :state
-	column :country
-	column :phone_number
-	column :email
-	column :office_hours
-    actions
-  end
-
-  form do |f|
-    f.inputs "Company Details" do
-      f.input :about_us
-      f.input :address_line_1
-      f.input :address_line_2
-      f.input :city
-      f.input :state
-      f.input :country
-      f.input :phone_number
-      f.input :email
-      f.input :office_hours
-    end
-    f.actions
+    	column :about_us
+    	column :address_line_1
+    	column :address_line_2
+    	column :city
+    	column :state
+    	column :country
+    	column :phone_number
+    	column :email
+    	column :office_hours
+      actions
   end
 end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def index
+  	@company = Company.find(1)
   end
 end
