2c0d55ec0eda4feaff3902d3bd091b4e68875262
sthpravin <sthpravin@gmail.com>
2016-09-13 23:47:02 +0545

    Address line 1 fixed again :D

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,19 +1,6 @@
 class Company < ApplicationRecord
   VALID_PHONE_NUMBER_REGEX = /\A[0-9\+][1]\s\d{3}-\d{3}-\d{4}\z/
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-<<<<<<< HEAD
-<<<<<<< HEAD
   validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX } 
-=======
-
-  validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  
->>>>>>> 0445c929b343a16f79743b9d4444595f80d1a3be
-=======
-
-  validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
->>>>>>> 8b2268832a4963ea4df788cdd6578fb1d7fa16bc
 end
