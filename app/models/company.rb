8c7b4cca89f138a58c1e56d571ad61c72df943a5
swojeet <swojeetkayastha@gmail.com>
2016-09-14 08:07:48 +0545

    Run bundle install locally

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,5 @@ class Company < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }  
 end
