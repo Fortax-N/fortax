75c4c71546300d87ee549b1ab221ce37bbede95f
swojeet <swojeetkayastha@gmail.com>
2016-09-15 12:54:26 +0545

    Rename online_estimate with online_estimates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #route for homepage
   root 'static_pages#index'
-  get '/online_estimate', to: 'online_estimate#index'
+  get '/online_estimates', to: 'online_estimates#index'
   get '/personal_information', to: 'personal_information#index'
 end
