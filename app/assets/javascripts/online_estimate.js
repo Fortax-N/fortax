7e37ecbbda521803f86516da26b10740d638cb5e
Rabin Gaire <rabingaire20@gmail.com>
2016-09-20 15:21:24 +0545

    Added more features on online_estimates form

--- app/assets/javascripts/online_estimate.js
+++ app/assets/javascripts/online_estimate.js
@@ -4,7 +4,6 @@ var initializeOnlineEstimate = function(){
   onlineEstimate = {
     index: 0,
     cost: 0,
-    price: 4,
     eFileFee: 2.99,
     totalCost: 0,
     initialize: function(){
@@ -16,24 +15,25 @@ var initializeOnlineEstimate = function(){
         $('#helperText').remove();
       }
     },
-    appendToTable: function(index, dropDownData, numOfForms) {
+    appendToTable: function(index, dropDownData, numOfForms, price) {
       $('#table-data').append(
         '<tr>' +
           '<td>'+ index + '</td>' +
           '<td>'+ dropDownData + '</td>' +
           '<td>'+ numOfForms + '</td>' +
+          '<td>'+ '$' + price + '</td>' +
         '</tr>'
       );
     },
     changeEstimate: function(){
       $('#errorMessage').css('display','none');
       $('#costDisplay').text( this.cost );
-      //rounding up to two places 
+      //rounding up to two places
       this.totalCost = Math.round((this.cost + this.eFileFee) * 100) / 100;
       $('#totalCost').text( this.totalCost );
     },
-    calculateCost: function(numOfForms) {
-      this.cost += numOfForms * this.price;
+    calculateCost: function(numOfForms,price) {
+      this.cost += numOfForms * price;
     },
     displayError: function(){
       $('#errorMessage').css('display','block');
@@ -43,15 +43,27 @@ var initializeOnlineEstimate = function(){
 
   onlineEstimate.initialize();
 
+  $('#form-select').on('change',function(){
+    var dropDownText = $('#form-select option:selected').text();
+    $('#numberOfForm').val(1);
+    if(dropDownText == "Student T2202")
+    {
+      $('#numberOfForm').attr("disabled","disabled");
+    } else {
+      $('#numberOfForm').removeAttr("disabled");
+    }
+  });
+
   $('#form-add').on('click',function(){
     var dropDownData = $('#form-select option:selected').val();
     var numOfForms = $('#numberOfForm').val();
+    var price = $('#form-select option:selected').data('cost');
     if((dropDownData != "") && (numOfForms != 0))
     {
       onlineEstimate.index++;
-      onlineEstimate.appendToTable(onlineEstimate.index, dropDownData, numOfForms);
+      onlineEstimate.appendToTable(onlineEstimate.index, dropDownData, numOfForms, price);
       onlineEstimate.initialize();
-      onlineEstimate.calculateCost(numOfForms);
+      onlineEstimate.calculateCost(numOfForms,price);
       onlineEstimate.changeEstimate();
     } else {
       onlineEstimate.displayError();
