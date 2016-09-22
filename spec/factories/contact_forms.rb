471ce0c8dcd88d689289bb9ed2f659ae31f895d2
sthpravin <sthpravin@gmail.com>
2016-09-23 01:51:18 +0545

    contact form factory girl fixed

--- spec/factories/contact_forms.rb
+++ spec/factories/contact_forms.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     f.name "Jon Shaw"
     f.email "jonshaw@gmail.com"
     f.message " Veniam veniam sit cupidatat mollit dolor proident. Ea est reprehenderit reprehenderit ullamco. Sunt dolore sint velit incididunt dolore reprehenderit ad sit. Do esse voluptate sit in consequat sint Lorem consectetur laboris elit ipsum. Fugiat excepteur dolor veniam sit velit aliquip laboris consectetur dolor incididunt sint proident."
+    f.nickname ""
   end
 end
