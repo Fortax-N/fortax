c3e881ec98361d03d69b27ecb6de5d6e19278826
Take Mouri <takehiromouri@gmail.com>
2016-09-23 13:04:41 +0545

    remove unnecessary helper specs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,4 @@ Rails.application.configure do
   end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-<<<<<<< HEAD
-=======
 end
->>>>>>> af8e4f4986edf4f065e6b5501f719dd179ecf586
