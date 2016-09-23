1fa9c01b6cd9d33032b153d6722ae0515318fedd
Take Mouri <takehiromouri@gmail.com>
2016-09-23 15:02:13 +0545

    online estimate js bug

--- app/assets/javascripts/online_estimate.js
+++ app/assets/javascripts/online_estimate.js
@@ -1,12 +1,52 @@
 var initializeOnlineEstimate = function(){
   var onlineEstimate;
+  var form;
+
+  form = {
+    hasSelectedStudentForm: false,
+    dropDownText: function(){
+      return $('#form-select option:selected').text();
+    },
+    numOfForms: function(){
+      return $('#numberOfForm').val();
+    },
+    price: function(){
+      return $('#form-select option:selected').data('cost');
+    },
+    addToForm: function(){      
+      if(form.dropDownText() !== "" && form.numOfForms() !== 0)
+      {
+        onlineEstimate.index++;        
+        onlineEstimate.appendToTable();
+        onlineEstimate.toggleHelperText();
+        onlineEstimate.calculateCost();
+        onlineEstimate.changeEstimate();
+        onlineEstimate.changeStatus();
+        form.disableButton();
+      } else {
+        onlineEstimate.displayError();
+      }      
+    },
+    isStudentForm: function(){
+      return (form.dropDownText() === "Student T2202");
+    },
+    disableButton: function(){
+      if (form.isStudentForm() === true) {
+        $('#form-add').off("click");
+        $('.addFormButton').prop("disabled", true);
+      }
+    },
+    enableButton: function(){
+      $("#form-add").removeAttr("disabled");
+    }
+  }
 
   onlineEstimate = {
     index: 0,
     cost: 0,
     eFileFee: 2.99,
-    totalCost: 0,
-    initialize: function(){
+    totalCost: 0,           
+    toggleHelperText: function(){
       var tableLength = $('#table-data tr').length;
       if (tableLength === 0) {
         $('#table-data').append('<p id="helperText">No Data Added</p>');
@@ -15,61 +55,52 @@ var initializeOnlineEstimate = function(){
         $('#helperText').remove();
       }
     },
-    appendToTable: function(index, dropDownData, numOfForms, price) {
+    appendToTable: function() {
       $('#table-data').append(
         '<tr>' +
-          '<td>'+ index + '</td>' +
-          '<td>'+ dropDownData + '</td>' +
-          '<td>'+ numOfForms + '</td>' +
-          '<td>'+ '$' + price + '</td>' +
+          '<td>'+ this.index + '</td>' +
+          '<td>'+ form.dropDownText() + '</td>' +
+          '<td>'+ form.numOfForms() + '</td>' +
+          '<td>'+ '$' + form.price() + '</td>' +
         '</tr>'
       );
     },
     changeEstimate: function(){
       $('#errorMessage').css('display','none');
       $('#costDisplay').text( this.cost );
-      //rounding up to two places
-      this.totalCost = Math.round((this.cost + this.eFileFee) * 100) / 100;
+      this.totalCost = Math.round(this.calculateTotalCost) / 100;
       $('#totalCost').text( this.totalCost );
     },
-    calculateCost: function(numOfForms,price) {
-      this.cost += (numOfForms * price);
+    calculateTotalCost: function(){
+      return (this.cost + this.eFileFee) * 100
+    },
+    calculateCost: function() {
+      this.cost += (form.numOfForms() * form.price());
     },
     displayError: function(){
       $('#errorMessage').css('display','block');
+    },
+    resetNumOfForms: function(){
+      $('#numberOfForm').val(1);
+    },
+    changeStatus: function(){
+      if (form.isStudentForm() === true) {
+        form.hasSelectedStudentForm = true;
+      }
     }
-
   }
 
-  onlineEstimate.initialize();
+  onlineEstimate.toggleHelperText();
 
   $('#form-select').on('change',function(){
-    var dropDownText = $('#form-select option:selected').text();
-    $('#numberOfForm').val(1);
-    if(dropDownText === "Student T2202")
+    onlineEstimate.resetNumOfForms();
+
+    if(form.hasSelectedStudentForm && form.isStudentForm())
     {
-      $('#numberOfForm').prop("disabled", true);
-      $('#form-add').on('click',function(){
-        $(this).off("click");
-      });
-    }
-    else
-    {
-      $('#form-add').on('click',function(){
-        var dropDownData = $('#form-select option:selected').val();
-        var numOfForms = $('#numberOfForm').val();
-        var price = $('#form-select option:selected').data('cost');
-        if(dropDownData != "" && numOfForms != 0)
-        {
-          onlineEstimate.index++;
-          onlineEstimate.appendToTable(onlineEstimate.index, dropDownData, numOfForms, price);
-          onlineEstimate.initialize();
-          onlineEstimate.calculateCost(numOfForms,price);
-          onlineEstimate.changeEstimate();
-        } else {
-          onlineEstimate.displayError();
-        }
-      });
+      form.disableButton();     
+    } else {
+      form.enableButton();
+      $('#form-add').on('click', form.addToForm);
     }
   });
 }
