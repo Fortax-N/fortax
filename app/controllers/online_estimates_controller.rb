f883c2c973643ca447186e04ffb06201324cf875
Rabin Gaire <rabingaire20@gmail.com>
2016-09-26 16:39:46 +0545

    Added mailer function

--- app/controllers/online_estimates_controller.rb
+++ app/controllers/online_estimates_controller.rb
@@ -5,7 +5,9 @@ class OnlineEstimatesController < ApplicationController
   end
 
   def create
-    NotifierMailer.sample_email(:form).deliver
+    NotifierMailer.mail_to_user(form_params).deliver
+    NotifierMailer.mail_to_admin(form_params).deliver
+    redirect_to online_estimates_path
   end
 
 
@@ -13,47 +15,47 @@ class OnlineEstimatesController < ApplicationController
 
   def form_params
     params.require(:form).permit(
-    :first_name
-    :last_name
-    :social_insurance
-    :date_of_birth(1i)
-    :date_of_birth(2i)
-    :date_of_birth(3i)
-    :status
-    :change_in_status_during_year
-    :date_of_change_of_status(1i)
-    :date_of_change_of_status(2i)
-    :date_of_change_of_status(3i)
-    :status_before_date
-    :gender
-    :address_line_1
-    :unit
-    :city
-    :province
-    :country
-    :postal_code
-    :sign_document
-    :phone_number
-    :email
-    :receive_notice_of_assessment_by_email
-    :register_for_direct_deposit
-    :branch
-    :financial_institution
-    :account
-    :spouse_name
-    :spouse_last_name
-    :spouse_date_of_birth
-    :spouse_social_insurance
-    :spouse_phone_number
-    :spouse_email
-    :residence
-    :property_tax_paid
-    :rent_paid
-    :child_name
-    :child_last_name
-    :child_date_of_birth(1i)
-    :child_date_of_birth(2i)
-    :child_date_of_birth(3i)
+    :first_name,
+    :last_name,
+    :social_insurance,
+    :date_of_birth,
+    :date_of_birth,
+    :date_of_birth,
+    :status,
+    :change_in_status_during_year,
+    :date_of_change_of_status,
+    :date_of_change_of_status,
+    :date_of_change_of_status,
+    :status_before_date,
+    :gender,
+    :address_line_1,
+    :unit,
+    :city,
+    :province,
+    :country,
+    :postal_code,
+    :sign_document,
+    :phone_number,
+    :email,
+    :receive_notice_of_assessment_by_email,
+    :register_for_direct_deposit,
+    :branch,
+    :financial_institution,
+    :account,
+    :spouse_name,
+    :spouse_last_name,
+    :spouse_date_of_birth,
+    :spouse_social_insurance,
+    :spouse_phone_number,
+    :spouse_email,
+    :residence,
+    :property_tax_paid,
+    :rent_paid,
+    :child_name,
+    :child_last_name,
+    :child_date_of_birth,
+    :child_date_of_birth,
+    :child_date_of_birth,
     :child_social_insurance
     )
   end
